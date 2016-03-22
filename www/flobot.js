a1021d8bd38ed097ca3127b585c74abe731722c2
Nick Moore <nick@mnemote.com>
2016-03-22 23:20:53 +1100

    new UI kinda working

--- www/flobot.js
+++ www/flobot.js
@@ -21,24 +21,41 @@ window.onload = function () {
         xhr.send();
     }
 
-    
     // EDGE
    
     var Edge = function(port_src, port_dst) {
-        this.port1 = port_src;
-        this.port2 = port_dst;
+        if (port_src && !port_dst) port_dst = port_src;
+        if (port_dst && !port_src) port_src = port_dst;
+
+        this.x1 = port_src.offset_x + port_src.node.geometry.x;
+        this.y1 = port_src.offset_y + port_src.node.geometry.y;
+        this.x2 = port_dst.offset_x + port_dst.node.geometry.x;
+        this.y2 = port_dst.offset_y + port_dst.node.geometry.y;
     }
 
     Edge.prototype.update = function() {
         // XXX TODO continue playing with this
-        var x1 = this.port1.get_geometry().x;
-        var y1 = this.port1.get_geometry().y;
-        var x2 = this.port2.get_geometry().x;
-        var y2 = this.port2.get_geometry().y;
-        var dy = Math.abs(y1 - y2) * 0.5 + 20;
-        var dx = (y2 > y1) ? Math.sign(x2 - x1) * dy : 0;
-        var spline_path = ['M', x1, y1, 'C', x1 + dx, y1 - dy, x2 - dx, y2 + dy, x2, y2].join(" ");
+        var dy = Math.abs(this.y1 - this.y2) * 0.5 + 20;
+        var dx = (this.y2 < this.y1) ? Math.sign(this.x2 - this.x1) * dy : 0;
+        var spline_path = [
+            'M', this.x1, this.y1,
+            'C', this.x1 + dx, this.y1 + dy,
+            this.x2 - dx, this.y2 - dy,
+            this.x2, this.y2
+        ].join(" ");
         this.svg_spline.setAttribute('d', spline_path);
+    }
+
+    Edge.prototype.move_src = function(delta_x, delta_y) {
+        this.x1 += delta_x;
+        this.y1 += delta_y;
+        this.update();
+    }
+
+    Edge.prototype.move_dst = function(delta_x, delta_y) {
+        this.x2 += delta_x;
+        this.y2 += delta_y;
+        this.update();
     }
 
     Edge.prototype.init = function(svg_element) {
@@ -47,14 +64,45 @@ window.onload = function () {
         svg_element.insertBefore(this.svg_spline, svg_element.firstChild);
     }   
     
+    Edge.prototype.deinit = function() {
+        this.svg_spline.remove();
+    }
+
     // PORT
 
-    var Port = function(node, label, offset_x, offset_y) {
+    var Port = function(node, label, is_input, offset_x, offset_y) {
         this.node = node;
         this.label = label;
+        this.is_input = is_input;
         this.offset_x = offset_x;
         this.offset_y = offset_y;
         this.edges = [];
+    }
+
+    Port.prototype.drag_init = function () {
+        if (this.is_input) this.remove_edges();
+        var new_edge = new Edge(this, null);
+        new_edge.init(this.node.prog.svg_element);
+        this.svg_circle.setAttribute('class', 'port drag');
+        new_edge.svg_spline.setAttribute('class', 'edge drag');
+        
+        return {
+            drag: function(delta_x, delta_y) {
+                if (this.is_input) new_edge.move_src(delta_x, delta_y);
+                else new_edge.move_dst(delta_x, delta_y);
+            }.bind(this),
+            done: function(target) {
+                this.svg_circle.setAttribute('class', 'port');
+                if (target && target.edges) {
+                    if (target.is_input) target.remove_edges();
+                    this.edges.push(new_edge);
+                    target.edges.push(new_edge);
+                    new_edge.svg_spline.setAttribute('class', 'edge');
+                } else {
+                    new_edge.deinit();
+                }    
+            }.bind(this)    
+        }
     }
 
     Port.prototype.init = function(svg_group) {
@@ -63,7 +111,7 @@ window.onload = function () {
         this.svg_circle.setAttribute('cx', this.offset_x);
         this.svg_circle.setAttribute('cy', this.offset_y);
         this.svg_circle.setAttribute('r', 12);
-        this.svg_circle._port = this;
+        this.svg_circle._target = this;
         svg_group.appendChild(this.svg_circle);
         if (this.label) {
             this.svg_label = document.createElementNS(svg_xmlns, 'text');
@@ -71,7 +119,7 @@ window.onload = function () {
             this.svg_label.setAttribute('class', 'port');
             this.svg_label.setAttribute('x', this.offset_x);
             this.svg_label.setAttribute('y', this.offset_y);
-            this.svg_label._port = this;
+            this.svg_label._target = this;
             svg_group.appendChild(this.svg_label);
         }
     }
@@ -80,13 +128,11 @@ window.onload = function () {
         this.edges.forEach(function (e) { e.update() });
     }
 
-    Port.prototype.get_geometry = function() {
-        return {
-            x: this.node.geometry.x + this.offset_x,
-            y: this.node.geometry.y + this.offset_y
-        }
+    Port.prototype.remove_edges = function() {
+        this.edges.forEach(function (e) { e.deinit(); });
+        this.edges = [];
     }
-
+    
     Port.prototype.create_edge = function(other) {
         var edge = new Edge(this, other);
         this.edges.push(edge);
@@ -97,16 +143,16 @@ window.onload = function () {
 
     // NODE
 
-    var Node = function(prog, json, num) {
+    var Node = function(prog, json) {
         this.prog = prog;
         this.json = json;
         this.input_ports = (json.inputs || []).map(function (p, n) {
-            return new Port(this, p, 150*(n+1)/(json.inputs.length+1), 0);
+            return new Port(this, p, true, 150*(n+1)/(json.inputs.length+1), 0);
         }, this);
         this.output_ports = (json.outputs || []).map(function (p, n) {    
-            return new Port(this, p, 150*(n+1)/(json.outputs.length+1), 50);
+            return new Port(this, p, false, 150*(n+1)/(json.outputs.length+1), 50);
         }, this); 
-        this.geometry = { x: (num % 4) * 200 + 50, y: (num >> 2) * 100 + 50 };
+        this.geometry = json.geometry || { x: 100, y: 100 };
     };
 
     Node.prototype.update = function() {
@@ -116,33 +162,32 @@ window.onload = function () {
         this.output_ports.forEach(function (p) { p.update(); });
     }
 
-    Node.prototype.init_drag = function (e) {
+    Node.prototype.drag_init = function() {
         this.svg_group.parentNode.appendChild(this.svg_group); // move to top
         this.svg_group.setAttribute('class', 'node drag');
-            
-        var drag_offset_x = this.geometry.x - e.screenX;
-        var drag_offset_y = this.geometry.y - e.screenY;
-        
-        var drag_move = function (e) {
-            this.geometry.x = drag_offset_x + e.screenX;
-            this.geometry.y = drag_offset_y + e.screenY;
-            this.update();
-            e.preventDefault();
-        }.bind(this);
-
-        var drag_end = function (e) {
-            this.svg_group.removeEventListener('mousemove', drag_move);
-            this.svg_group.removeEventListener('mouseup', drag_end);
-            this.svg_group.removeEventListener('mouseleave', drag_end);
-            this.svg_group.setAttribute('class', 'node');
-            e.preventDefault();
-        }.bind(this);
-        
-        this.svg_group.addEventListener('mousemove', drag_move);
-        this.svg_group.addEventListener('mouseup', drag_end);
-        this.svg_group.addEventListener('mouseleave', drag_end);
+    
+        return {
+            drag: function(delta_x, delta_y) {
+                this.geometry.x += delta_x;
+                this.geometry.y += delta_y;
+                this.update();
+                this.input_ports.forEach(function (p) {
+                    p.edges.forEach(function (e) {
+                        e.move_dst(delta_x, delta_y);
+                    });
+                });
+                this.output_ports.forEach(function (p) {
+                    p.edges.forEach(function (e) {
+                        e.move_src(delta_x, delta_y);
+                    });
+                });
+            }.bind(this),
+            done: function () {
+                this.svg_group.setAttribute('class', 'node');
+            }.bind(this)
+        } 
     }
-
+    
     Node.prototype.init_port = function (port) {
         port.init(this.svg_group);
     }
@@ -150,7 +195,7 @@ window.onload = function () {
     Node.prototype.init = function(svg_element) {
         this.svg_group = document.createElementNS(svg_xmlns, 'g');
         this.svg_group.setAttribute('class', 'node');
-        this.svg_group._node = this;
+        this.svg_group._target = this;
         svg_element.appendChild(this.svg_group);
         this.svg_rect = document.createElementNS(svg_xmlns, 'rect');
         this.svg_group.appendChild(this.svg_rect);
@@ -159,10 +204,8 @@ window.onload = function () {
         this.svg_label.textContent = this.json.label;
         this.svg_label.setAttribute('x', 75);
         this.svg_label.setAttribute('y', 25);
-        this.svg_label._node = this;
+        this.svg_label._target = this;
         this.svg_group.appendChild(this.svg_label);
-        
-        this.svg_group.addEventListener('mousedown', this.init_drag.bind(this));
         
         this.update();
 
@@ -198,44 +241,49 @@ window.onload = function () {
 
         this.svg_element.addEventListener('dblclick', new_node.bind(this));
 
-        this.svg_element.addEventListener('mousedown', function (e) {
-            if (e.target._port) {
-            
-            } 
-            else if (e.target._node) {
-                
-            }
-            else {
+        var drag_target = null;
+        var drag_offset_x = 0;
+        var drag_offset_y = 0;
 
-
+        function drag_start(e) {
+            if (e.target._target) {
+                drag_target = e.target._target.drag_init();
+                drag_offset_x = e.screenX;
+                drag_offset_y = e.screenY;
             }
             e.preventDefault();
-        }.bind(this));
+        }
 
-/*        this.svg_element.addEventListener('mousemove', function (e) {
+        function drag_move(e) {
+            if (drag_target) {
+                drag_target.drag(
+                    e.screenX - drag_offset_x,
+                    e.screenY - drag_offset_y
+                )
+                drag_offset_x = e.screenX;
+                drag_offset_y = e.screenY;
+            }
+            e.preventDefault();
+        }
 
-        }.bind(this));
+        function drag_end(e) {
+            if (drag_target) {
+                drag_target.done(e.target._target);
+                drag_target = null;
+            }
+            e.preventDefault();
+        }
 
-        this.svg_element.addEventListener('mouseup', function (e) {
-
-        }.bind(this));
-*/
-
+        this.svg_element.addEventListener('mousedown', drag_start.bind(this));
+        this.svg_element.addEventListener('mousemove', drag_move.bind(this));
+        this.svg_element.addEventListener('mouseup', drag_end.bind(this));
+        this.svg_element.addEventListener('mouseleave', drag_end.bind(this));
     }
 
     ajax_get('builtins.json', function (data) {
         var json = JSON.parse(data);
         var prog = new Prog(json);
         prog.init(document.body);
-
-        prog.nodes[2].input_ports[0].create_edge(
-            prog.nodes[0].output_ports[0]
-        );
-
-        prog.nodes[3].input_ports[0].create_edge(
-            prog.nodes[0].output_ports[1]
-        );
-
 
     });
 }
